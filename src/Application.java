3acb0f19377e4f0508aa0a3696e6f149e124d8ee
Raymond Loman <raymond@carpago.nl>
2020-01-09 10:36:32 +0100

    Initial load

---
+++ src/Application.java
@@ -0,0 +1,36 @@
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+
+        System.out.println(Long.MAX_VALUE);
+
+        byte age = 127;
+        short age2 = 32767;
+        int age3 = 404894949;
+
+        // float example
+        float f1 = 3.5f;
+
+
+        double d1 = 3.5;
+
+        double pi = 3.1415926535;
+
+        char c = 'S';
+        String s = "Sam";
+
+        boolean raining = false;
+
+        boolean old = age > 100;
+
+        System.out.println(old);
+
+        if (raining) {
+            // drive();
+        } else {
+            // walk();
+        }
+    }
+}
---
+++ src/Car.java
@@ -0,0 +1,8 @@
+public class Car {
+
+     String licensePlate;
+     String color;
+     String brand;
+     double mileage;
+
+}
