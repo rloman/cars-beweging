8af13486d36539158aa3e78a7d0e535a41d72d55
Raymond Loman <raymond@carpago.nl>
2020-01-10 09:54:31 +0100

    Explain local vars versus instance vars (fields). And add Questions answered friday 2020-01-10

---
+++ src/com/capgemini/carcase/answers/Monkey.java
@@ -0,0 +1,47 @@
+package com.capgemini.carcase.answers;
+
+public class Monkey {
+
+    private int age;
+
+    public static void main(String[] args) {
+        System.out.println("This should run!");
+
+
+        Monkey m = new Monkey();
+        m.age = 14;
+        System.out.println(m.age);
+
+        int currentAge = m.birthDay();
+        System.out.println(currentAge);
+
+        System.out.println(m.age);
+
+        double myAge = 55.25;
+
+        System.out.println(m.add(3,4));
+    }
+
+    public int add(int a, int b) {
+        return a+b;
+    }
+
+
+    public int birthDay() {
+        age = age + 1;
+
+        System.out.println("Vrijdag");
+
+        int whatever = 10000;
+
+
+
+        return 100;
+    }
+
+
+}
+
+class Banana {
+
+}
---
+++ src/com/capgemini/carcase/model/Wheel.java
@@ -0,0 +1,33 @@
+package com.capgemini.carcase.model;
+
+public class Wheel {
+
+    // field. instance variable. attribute.
+    private int size; // filled with a default value when created
+
+
+
+    public void fit() {
+
+
+        // local variable. c people sometimes call this an "automatic".
+        int length = 0; // should be initialized
+
+        System.out.println(length);
+
+    }
+
+
+    public void doo() {
+
+
+        System.out.println(size);
+
+        // not reachable.
+        System.out.println();
+
+    }
+
+
+
+}
