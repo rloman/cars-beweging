54cadecb76a58572743f53cacc3cbb4abc3895d5
Raymond Loman <raymond@carpago.nl>
2020-01-24 10:24:08 +0100

    Finish assignment interface::Panasonic

--- src/com/capgemini/interfaces/Panasonic.java
+++ src/com/capgemini/interfaces/Panasonic.java
@@ -5,5 +5,23 @@ public class Panasonic implements Television {
     private boolean on;
     private int currentChannel;
 
+    @Override
+    public boolean on() {
+        return false;
+    }
 
+    @Override
+    public void off() {
+
+    }
+
+    @Override
+    public int switchChannel(int newChannel) {
+        return 0;
+    }
+
+    @Override
+    public int getCurrentChannel() {
+        return 0;
+    }
 }
