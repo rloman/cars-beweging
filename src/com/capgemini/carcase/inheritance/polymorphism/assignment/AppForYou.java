e70b83f667c6b15dbd0c8deada9b5416371b296a
Raymond Loman <raymond@carpago.nl>
2020-01-24 09:33:18 +0100

    Add abstract class assignment code

---
+++ src/com/capgemini/carcase/inheritance/polymorphism/assignment/AppForYou.java
@@ -0,0 +1,12 @@
+package com.capgemini.carcase.inheritance.polymorphism.assignment;
+
+public class AppForYou {
+
+    public static void main(String[] args) {
+        Human b = new BaseballPlayer();
+        b.walk();
+    }
+
+}
+
+
---
+++ src/com/capgemini/carcase/inheritance/polymorphism/assignment/BaseballPlayer.java
@@ -0,0 +1,12 @@
+package com.capgemini.carcase.inheritance.polymorphism.assignment;
+
+public class BaseballPlayer extends Human {
+
+
+    @Override
+    public void walk() {
+
+        System.out.println("Walking baseballplayer ... ");
+
+    }
+}
